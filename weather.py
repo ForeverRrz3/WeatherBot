9efcc46c2f435bfca2e3ef84dad29123756b3986
ForeverRrz3 <sashashovyrin@gmail.com>
2025-07-31 22:31:46 +0300

    Update weather.py
    fix

--- weather.py
+++ weather.py
@@ -1,39 +1,34 @@
-from time import *
-from translate import *
 from math import *
-from bs4 import BeautifulSoup
 from requests import *
 
-def lat_lon_town(name:str):
-    t = Translator(from_lang="ru",to_lang="en")
-    town = t.translate(name)
-    print(town)
-    url = f"http://api.openweathermap.org/geo/1.0/direct?q={name}&limit=5&appid=01c132b3eb49bd5fc9483e858314c928"
-    response = get(url).json()[0]
 
-    lat = response["lat"]
-    lon = response["lon"]
-
-
-
-    return lat,lon
 
 def weather_town_one_day(town:str):
-    lat,lon = lat_lon_town(town)
-    t = Translator(to_lang="ru",from_lang="en")
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={town}&appid=01c132b3eb49bd5fc9483e858314c928&units=metric"
-    response = get(url).json()
-    print(response)
+
+
+    params = {"q": town,
+            "appid":"API KEY",
+            "units":"metric",
+            "lang":"ru"}
+    url = f"https://api.openweathermap.org/data/2.5/weather"
+    response = get(url, params=params).json()
+    
     weather = response["main"]
-    feels, temp  = ceil(weather["feels_like"]),ceil(weather["temp"])
-    sky = t.translate(response["weather"][0]["description"])
+    feels, temp, sky  = ceil(weather["feels_like"]),ceil(weather["temp"]),response["weather"][0]["description"]
+    
     return f"Температура ощущается как {"+"+str(feels) if feels>=0 else "-"+str(feels)}, реальная - {"+"+str(temp) if temp>=0 else "-"+str(temp)}. Погода - {sky}"
 
 def weather_days(town:str):
-    t = Translator(to_lang="ru",from_lang="en")
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={town}&appid=01c132b3eb49bd5fc9483e858314c928&units=metric&lang=ru"
-    response = get(url)
+    params = {"q": town,
+            "appid":"API KEY",
+            "units":"metric",
+            "lang":"ru"}
+    
+
+    url = f"http://api.openweathermap.org/data/2.5/forecast"
+    response = get(url,params=params)
     data = response.json()
+
     back = []
     for i in data["list"]:
         if "12:00:00" in i["dt_txt"]:
@@ -41,7 +36,5 @@ def weather_days(town:str):
             sky = i["weather"][0]["description"]
             back.append(f"- {i["dt_txt"]} температура ощущается как {"+"+str(feels) if feels>=0 else "-"+str(feels)}, реальная - {"+"+str(temp) if temp>=0 else "-"+str(temp)}. Погода - {sky}\n")
     return back
-
-# 01c132b3eb49bd5fc9483e858314c928
 
 
